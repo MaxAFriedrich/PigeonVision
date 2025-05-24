0f2372977a22f1255a0ba7dd5f473cf7970ba2e7
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2025-05-24 16:44:28 +0100

    main call wrapper

--- pigeonvision/__init__.py
+++ pigeonvision/__init__.py
@@ -1,3 +1,49 @@
-from pigeonvision import persistent
+from pigeonvision import persistent, heuristics, validate
 
 persistent.load()
+
+
+def certainty_to_estimate_word(certainty: float) -> str:
+    if certainty == 1.0:
+        return "We are certain that the item is malicious."
+    elif 0.87 <= certainty < 1.0:
+        return "We are almost certain that the item is malicious."
+    elif 0.60 <= certainty < 0.87:
+        return "We think it is probable that the item is malicious."
+    elif 0.40 <= certainty < 0.60:
+        return ("We think chances are about even as to whether the item is "
+                "malicious or not.")
+    elif 0.20 <= certainty < 0.40:
+        return ("We think there is a realistic possibility that the item is "
+                "malicious.")
+    elif 0.01 <= certainty < 0.20:
+        return "We think it is unlikely that the item is malicious."
+    elif certainty == 0.0:
+        return "We are certain that the item is not malicious."
+    else:
+        raise ValueError(f"Unknown certainty value: {certainty}")
+
+
+def main(query: str) -> (str, str):
+    query_type, validation_outcome, normalised_query = validate.validate_query(query)
+
+    if validation_outcome == validate.ValidationOutcome.INVALID:
+        return "This is not a valid query.", "INVALID"
+    elif validation_outcome == validate.ValidationOutcome.WHITELIST:
+        return ("This domain is certainly not malicious.",
+                "The item you queried is whitelisted and therefore we do not "
+                "think it is malicious.")
+    elif validation_outcome != validate.ValidationOutcome.UNCERTAIN:
+        raise ValueError("Unknown validation outcome.")
+
+    if query_type == validate.QueryType.UNKNOWN:
+        return (
+            "This query is in an unknown format.",
+            "Valid formarts are: "
+            "IP address, domain name, email address, URL, MD5 or SHA1 hash.")
+
+    certainty, message = heuristics.run(query, query_type)
+    certainty_word = certainty_to_estimate_word(certainty)
+    message = (f"<h1>We think that there is a {certainty * 100:.2f}% chance "
+               f"that the item is malicious because:</h1>") + message
+    return certainty_word, message
--- pigeonvision/__main__.py
+++ pigeonvision/__main__.py
@@ -1,6 +1,12 @@
-from pigeonvision import persistent
-
-persistent.load()
+import pigeonvision
+import sys
 
 if __name__ == "__main__":
-    print("Hello, Pigeon Vision!")
+    # get the first argument from the command line
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+        short, explanation = pigeonvision.main(query)
+        print(short)
+        print(explanation)
+    else:
+        print("Usage: python -m pigeonvision <query>")
