8788920aaaf83af0a98bb4fe0850c3b5e4974a19
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2025-05-25 18:20:37 +0100

    extract domains from urls and emails

--- pigeonvision/heuristics/spamhaus/__init__.py
+++ pigeonvision/heuristics/spamhaus/__init__.py
@@ -1,18 +1,18 @@
-import requests
+import logging
+import math
 import os
 import time
-import math
-import logging
 
+import requests
 from dotenv import load_dotenv
 
-from pigeonvision.heuristics.base.result import Result
 from pigeonvision.heuristics.base import Heuristic
-from pigeonvision.validate.utils import QueryType
+from pigeonvision.heuristics.base.result import Result
 from pigeonvision.heuristics.spamhaus.spamhaus_tags import tag_messages
+from pigeonvision.validate.utils import QueryType, extract_domain
+
 
 class spamhaus(Heuristic):
-
     auth_token = ''
     auth_attempts = 0
 
@@ -30,15 +30,18 @@ class spamhaus(Heuristic):
 
         spamhaus.auth_attempts += 1
 
-        if spamhaus.auth_attempts > 3: raise RuntimeError("Spamhaus authentication attempt limit reached")
+        if spamhaus.auth_attempts > 3: raise RuntimeError(
+            "Spamhaus authentication attempt limit reached")
 
-        data = f'{{"username": "{os.environ["SPAMHAUS_USER"]}", "password": "{os.environ["SPAMHAUS_PASS"]}", "realm":"intel"}}'
+        data = (f'{{"username": "{os.environ["SPAMHAUS_USER"]}", "password": '
+                f'"{os.environ["SPAMHAUS_PASS"]}", "realm":"intel"}}')
 
         res = requests.post('https://api.spamhaus.org/api/v1/login', data=data)
 
         if res.status_code == 200:
-
-            spamhaus.logger.debug("Spamhaus authentication successful in %i attempts", spamhaus.auth_attempts)
+            spamhaus.logger.debug(
+                "Spamhaus authentication successful in %i attempts",
+                spamhaus.auth_attempts)
             spamhaus.auth_attempts = 0
             spamhaus.auth_token = res.json()["token"]
 
@@ -46,14 +49,20 @@ class spamhaus(Heuristic):
     def fetch_domain(query: str) -> Result:
         headers = {"Authorization": f"Bearer {spamhaus.auth_token}"}
 
-        spamhaus.logger.debug("Querying https://api.spamhaus.org/api/intel/v2/byobject/domain/%s", query)
+        spamhaus.logger.debug(
+            "Querying https://api.spamhaus.org/api/intel/v2/byobject/domain/%s",
+            query)
 
         msg = ("<h2>Spamhaus</h2>"
-        "<p>Spamhaus provides a score for domain reputation " 
-        "where 0 is neutral, positive numbers are good and negative are bad. "
-        "We've done some curve fitting to turn this score into a confidence interval. <br><br>")
+               "<p>Spamhaus provides a score for domain reputation "
+               "where 0 is neutral, positive numbers are good and negative "
+               "are bad. "
+               "We've done some curve fitting to turn this score into a "
+               "confidence interval. <br><br>")
 
-        res = requests.get(f"https://api.spamhaus.org/api/intel/v2/byobject/domain/{query}", headers=headers)
+        res = requests.get(
+            f"https://api.spamhaus.org/api/intel/v2/byobject/domain/{query}",
+            headers=headers)
 
         score = int(res.json().get('score', 0))
         spamhaus.logger.debug("Spamhaus score: %i", score)
@@ -62,8 +71,10 @@ class spamhaus(Heuristic):
         normalised = (-(score / 2) / math.sqrt(25 + (score ** 2))) + 0.5
         spamhaus.logger.debug("Normalised percentage: %i", normalised)
 
-        msg += (f"Spamhaus give this a score of {score}, which we've turned "
-                "into a malicious confidence value of {:.2f}<br><br>".format(normalised))
+        msg += (
+            f"Spamhaus give this a score of {score}, which we've turned "
+            f"into a malicious confidence value of {normalised:.2f}<br><br>"
+        )
 
         for tag in res.json().get('tags', []):
             spamhaus.logger.debug("Found tag %s", tag)
@@ -73,24 +84,29 @@ class spamhaus(Heuristic):
         msg += '</p>'
 
         return Result(
-                certainty = normalised,
-                raw = res.text,
-                timestamp = time.time(),
-                message = msg
-                )
+            certainty=normalised,
+            raw={"data": res.text},
+            timestamp=time.time(),
+            message=msg
+        )
 
     @staticmethod
     def fetch(query: str, query_type: QueryType) -> Result:
+        if query_type in [QueryType.URL, QueryType.EMAIL]:
+            query = extract_domain(query)
 
         spamhaus.logger.info("Starting spamhaus heuristic")
 
-        if spamhaus.auth_token == '': spamhaus.acquire_auth()
+        if spamhaus.auth_token == '':
+            spamhaus.acquire_auth()
 
         return spamhaus.fetch_domain(query)
 
     @staticmethod
     def allowed_query_types() -> list[QueryType]:
-        return [QueryType.MD5, QueryType.SHA1, QueryType.URL, QueryType.DOMAIN]
+        return [QueryType.URL, QueryType.DOMAIN,
+                QueryType.EMAIL]
+
 
 '''
     class TestHeuristic(Heuristic):
